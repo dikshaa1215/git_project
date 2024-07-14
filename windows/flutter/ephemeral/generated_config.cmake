34ee187fd0a902fabdc1abb35a7b2d155cd27c92
Diksha <dikshaverma2452@gmail.com>
2024-07-14 23:40:34 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\hp\\Desktop\\developer\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\hp\\Desktop\\Fltr_Apps\\meals" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\hp\\Desktop\\developer\\flutter"
+  "PROJECT_DIR=C:\\Users\\hp\\Desktop\\Fltr_Apps\\meals"
+  "FLUTTER_ROOT=C:\\Users\\hp\\Desktop\\developer\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\hp\\Desktop\\Fltr_Apps\\meals\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\hp\\Desktop\\Fltr_Apps\\meals"
+  "FLUTTER_TARGET=C:\\Users\\hp\\Desktop\\Fltr_Apps\\meals\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NWVhZTY4NjRiMjk2ZGQ5ZjQzYjJjYzc1NzdlYzI1NmU1YzMyYThkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\hp\\Desktop\\Fltr_Apps\\meals\\.dart_tool\\package_config.json"
+)
